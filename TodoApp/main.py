ad81429bd4404aecca460bc46ebd982eea452406
thehakohub <your-email@example.com>
2026-09-17 16:00:09 +0530

    updated changes

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import models
 from routers import auth, todos, admin, users
-from database import engine,Base
+from database import engine, Base
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # or restrict to your frontend domain
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["Authorization", "Content-Type"],
+)
+
 
 Base.metadata.create_all(bind=engine)
 
@@ -11,5 +21,3 @@ app.include_router(admin.router)
 app.include_router(auth.router)
 app.include_router(todos.router)
 app.include_router(users.router)
-
-
